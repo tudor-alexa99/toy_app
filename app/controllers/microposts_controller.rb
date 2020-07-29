af1ce0ed704304671acb193f19196471f51a8ca8
Tudor Alexa <tudor.alexa99@gmail.com>
2020-07-29 11:37:24 +0300

    Finish toy app

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,8 +1,22 @@
 class MicropostsController < ApplicationController
+  before_action :set_micropost, only: [:show, :edit, :update, :destroy]
+
   def new
+    @micropost = Micropost.new
   end
 
   def create
+    @micropost = Micropost.new(micropost_params)
+
+    respond_to do |format|
+      if @micropost.save
+        format.html { redirect_to @micropost, notice: 'Micropost successfully created' }
+        format.json { render :show, status: :created, location: @micropost }
+      else
+        format.html { render :new }
+        format.json { render json: @micropost.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
@@ -18,5 +32,16 @@ class MicropostsController < ApplicationController
   end
 
   def index
+    @microposts = Micropost.all
+  end
+
+  private
+
+  def set_micropost
+    @micropost = Micropost.find(params[:id])
+  end
+
+  def micropost_params
+    params.require(:micropost).permit(:content, :user_id)
   end
 end
